30a254fe2676a2a380c6c348a35641dbca41493b
FutureApple <pghkipy@outlook.com>
2019-11-03 10:37:06 +0800

    1106第2次修改

--- 1106/main.cpp
+++ 1106/main.cpp
@@ -1,4 +1,13 @@
-#include<iostream>
-#include<cstdio>
+#include<bits/stdc++.h>
 
 using namespace std;
+
+int main(){
+	double r,h,c,d;
+	cin>>h>>r;
+	c = 3.14;
+	d = 2*c*r*(r+h);
+	printf("%.2lf",d);
+	return 0;
+}
+
